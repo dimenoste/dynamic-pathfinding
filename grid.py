33681322ccb68985b517037c1fdf78bbd7254bf9
dimenoste <65079536+dimenoste@users.noreply.github.com>
2026-03-17 16:38:24 +0100

    wip

--- grid.py
+++ grid.py
@@ -2,22 +2,41 @@ import numpy as np
 import queue
 
 
-# create coordinates matrix 
-coords =  [(4, 10), (21, 10), (21, 1), (4, 1), 
-           (16, 6), (9, 6),  (13, 9), (12, 9), 
-           (20, 9), (5, 9), (1, 1), (24, 1), (7, 6), 
-           (18, 6), (11, 6), (14, 6), (9, 10), 
-           (16, 10), (5, 11), (20, 11)]
+# create coordinates matrix
+coords = [
+    (4, 10),
+    (21, 10),
+    (21, 1),
+    (4, 1),
+    (16, 6),
+    (9, 6),
+    (13, 9),
+    (12, 9),
+    (20, 9),
+    (5, 9),
+    (1, 1),
+    (24, 1),
+    (7, 6),
+    (18, 6),
+    (11, 6),
+    (14, 6),
+    (9, 10),
+    (16, 10),
+    (5, 11),
+    (20, 11),
+]
 
 # snake
-snake1 = ['0', '5,10:5,11:5,12']
-snake2 = ['1', '10,3:10,4:10,5']
+snake1 = ["0", "5,10:5,11:5,12"]
+snake2 = ["1", "10,3:10,4:10,5"]
 
 parts_coords_str = snake1[1].split(":")
 print(parts_coords_str)
 
-parts_coords = [tuple ( map (int, parts_coords_str[x].split(",") ) ) 
-                for x in range(len(parts_coords_str)) ] 
+parts_coords = [
+    tuple(map(int, parts_coords_str[x].split(",")))
+    for x in range(len(parts_coords_str))
+]
 
 print(parts_coords)
 
@@ -39,55 +58,53 @@ print(parts_coords)
 #     grid[x][y] = 1
 
 
-class Grid2d():
-    def __init__(self, coords, height, width):
-        self.width = width
-        self.height = height
-        self.grid = np.zeros((width, height), int)
-        self.coords = coords
-    
-    def fill_grid(self, value):
-        for x,y in self.coords:
-            grid[x][y] = 1
-
 def get_coords_input_parts(parts_coords_str):
-    return [tuple ( map (int, parts_coords_str[x].split(",") ) ) 
-                for x in range(len(parts_coords_str)) ]
+    return [
+        tuple(map(int, parts_coords_str[x].split(",")))
+        for x in range(len(parts_coords_str))
+    ]
 
 
-class Snake():
+class Snake:
     def __init__(self, snake_input):
         # snake_input = ['0', '5,10:5,11:5,12']
         self.body = get_coords_input_parts(snake_input[1].split(":"))
-        self.id  = int(snake_input[0])
-    
+        self.id = int(snake_input[0])
+
     def update_body(self, snake_input):
         self.body = get_coords_input_parts(snake_input[1].split(":"))
-    
+
     def get_coords_parts(self, part_number=None):
-        if part_number is not None and part_number <= len(self.body) and part_number >= 0:
+        if (
+            part_number is not None
+            and part_number <= len(self.body)
+            and part_number >= 0
+        ):
             print("from class ", self.body[part_number])
-            return  self.body[part_number]
+            return self.body[part_number]
         else:
             return self.body
-        
+
     def get_head(self):
         return self.body[0]
-    
+
     def get_tail(self):
         return self.body[-1]
-    
-snake_input = ['0', '5,10:5,11:5,12']
+
+
+snake_input = ["0", "5,10:5,11:5,12"]
 snake = Snake(snake_input)
 
 print("coordinates of snake :", snake.body)
 print("snake id :", snake.id)
 print("coordinates of part at index 2 :", snake.get_coords_parts(2))
-print("all body parts coordinates :", snake.get_coords_parts()) ## no number part passed as parameter => get the coordinates of all parts body
+print(
+    "all body parts coordinates :", snake.get_coords_parts()
+)  ## no number part passed as parameter => get the coordinates of all parts body
 print("head   coord :", snake.get_head())
 print("tail   coord :", snake.get_tail())
 
-snake_input = ['0', '5,10:5,11:5,12:5,13'] ## snake ate a power source and grows
+snake_input = ["0", "5,10:5,11:5,12:5,13"]  ## snake ate a power source and grows
 snake.update_body(snake_input)
 print("new tail   coord", snake.get_tail())
 
@@ -96,7 +113,7 @@ print("new tail   coord", snake.get_tail())
 #         neighbors = []
 #         x_curr, y_curr = current
 #         if  ((0 > x_curr)  or (x > self.width)):
-#             return 
+#             return
 #         elif ((0 > x_curr)  or (x > self.height)):
 #             return
 #         if (x_curr + 1 <= width):
@@ -107,15 +124,7 @@ print("new tail   coord", snake.get_tail())
 #             coords.add((x_curr, y_curr + 1))
 #         if (y_curr - 1 >= 0):
 #             coords.add((x_curr , y_curr - 1))
-    
 
-
-        
-            
-
-        
-
-    
 
 # print(grid)
 
@@ -135,11 +144,9 @@ print("new tail   coord", snake.get_tail())
 #     return coords_neighbors
 
 
-
 # while not frontier.empty():
 #     current = frontier.get()
 #     for next in neighbors(current):
 #         if not next in reached:
 #             frontier.put(next)
 #             reached.add(next)
-
--- pathfinding.py
+++ pathfinding.py
@@ -1,45 +1,114 @@
 from queue import Queue
 import numpy as np
+import sys
+from typing_extensions import TypeVarTuple, Unpack
+
+MARKER_GOAL = "G"
+MARKER_START = "S"
+MARKER_OBSTACLE = "X"
 
 
-start = (1, 2)
-
-frontier = Queue()
-frontier.put(start)
-came_from = dict()
-
-came_from[start] = None
-
-class graph():
-    def __init__(self, rows, cols):
+class Grid:
+    def __init__(
+        self,
+        cols: int,
+        rows: int,
+        start: tuple[int, int],
+        goal: tuple[int, int],
+        obstacles: set[tuple[int, int]],
+    ):
         self.cols = cols
         self.rows = rows
-        self.grid =  np.zeros((self.width, self.height), int)
+        self.start = start
+        self.goal = goal
+        self.obstacles = obstacles
+        self.grid = self.create_grid()
 
-    def fill_grid(self):
-        for i in range(rows):   
-    row = []
+    def create_grid(self) -> np.ndarray:
+        grid = np.zeros((self.cols, self.rows), dtype=object)
+        x_start, y_start = self.start
+        x_goal, y_goal = self.goal
+        grid[x_start][y_start] = MARKER_START
+        grid[x_goal][y_goal] = MARKER_GOAL
+        grid = self.add_obstacles_to_grid(grid)
+        return grid
 
-    
-    for j in range(col):
-        row.append(int(input()))    # user input for rows
-    matrix.append(row)
-    def add_obstacle(self.grid):
-        for i
+    def add_obstacles_to_grid(self, grid: np.ndarray) -> np.ndarray:
+        for x, y in self.obstacles:
+            grid[x][y] = MARKER_OBSTACLE
+        return grid
 
-    def neighbors(current):
-        curr_x, curr_y = current
-        return [(curr_x + 1, curr_y), (curr_x , curr_y + 1), 
-                (curr_x - 1, curr_y), (curr_x , curr_y - 1)]
-    
-    def display(origin, goal):
+    def is_in_grid(self, current: tuple[int, int]) -> bool:
+        first_row, first_col = 0
+        last_row = self.rows - 1
+        last_col = self.cols - 1
+        return (first_row <= current[1] <= last_row) and (
+            first_col <= current[0] <= last_col
+        )
+
+    # def add_points(a :tuple[int,int], b : tuple[int,int]) -> tuple[int,int]:
+    #     return (a[0]+b[0], a[1]+b[1])
+
+    def get_neighbors(self, current: tuple[int, int]) -> set[tuple[int, int]] | None:
+
+        neighbors = set()
+        x, y = current
+        left = (x - 1, y)
+        right = (x + 1, y)
+        down = (x, y + 1)
+        up = (x, y - 1)
+
+        if not self.is_in_grid(current):
+            return None
+
+        if self.is_in_grid(left):
+            set.add(left)
+        if self.is_in_grid(right):
+            set.add(right)
+        if self.is_in_grid(left):
+            set.add(left)
+        if self.is_in_grid(left):
+            set.add(left)
+
+        return None
+
+    def display(self) -> None:
+        for row in range(self.rows):
+            for col in range(self.cols):
+                print(self.grid[row][col], end=" ")
+            print()
 
 
+def main() -> int:
+
+    ## data given
+    start = (0, 0)
+    goal = (4, 6)
+    rows = 5
+    cols = 7
+    obstacles = {(1, 5), (1, 6), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5)}
+
+    ## cases to visit
+    is_visited = set()
+    is_visited.add(start)
+    print(is_visited)
+
+    to_visit_queue: Queue = Queue()
+    to_visit_queue.put(start)
+
+    curr = to_visit_queue.get()
+    if curr is None:
+        print("no path to goal")
+        return 1
+
+    # get_neighbors
+
+    ## grid creaction
+    grid = Grid(rows, cols, start, goal, obstacles)
+    grid.display()
+
+    return 0
 
 
-while not frontier.empty():
-    current = frontier.get()
-    for next in graph.neighbors(current):
-        if next not in came_from:
-            frontier.put(next)
-            came_from[next] = current
+if __name__ == "__main__":
+    main()
